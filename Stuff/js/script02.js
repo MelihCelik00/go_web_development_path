9286915980ea1c3dc61618534d15b7f6b7bde3d0
MelihCelik00 <melihsafa.c@gmail.com>
2022-02-18 23:29:41 +0300

    domdom kursunu

--- Stuff/js/script02.js
+++ Stuff/js/script02.js
@@ -5,6 +5,10 @@ console.log(h2.innerHTML);
 console.log(h2.innerHTML = "Change Lorem Ipsum");
 console.log(h2.innerHTML);
 
-let link = document.querySelector("ul#listID>li>a");
-link.innerHTML += " --- changing of link information"
+// let link = document.querySelector("ul#listID>li>a");
+let link = document.querySelector("#linkID");
+// link.innerHTML += " --- changing of link information"
+link.innerHTML = "Go to Google!"
 console.log(link.innerHTML);
+link.style.color = "red"; // add css style tag to the html tag
+link.classList.add('btn'); // add class to the html tag
