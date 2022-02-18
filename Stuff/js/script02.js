d66b528890b7c74e959b45667f833308f3482535
MelihCelik00 <melihsafa.c@gmail.com>
2022-02-18 21:35:53 +0300

    started to learn DOMs

---
+++ Stuff/js/script02.js
@@ -0,0 +1,7 @@
+// Let's work with Document Object Model which means DOMs, vamos!
+
+let h2 = document.getElementById("title-header-2");
+console.log(h2.innerHTML);
+console.log(h2.innerHTML = "Change Lorem Ipsum");
+console.log(h2.innerHTML);
+
