80ce7c92e43b92a1b55dab20e407e13539a14c0a
MelihCelik00 <melihsafa.c@gmail.com>
2022-02-21 19:56:39 +0300

    added form javascript code

---
+++ Stuff/js/script06-forms.js
@@ -0,0 +1,8 @@
+let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener("submit", formSubmit);
+
+function formSubmit(event){
+    event.preventDefault();
+    console.log("operation successful")
+
+}
