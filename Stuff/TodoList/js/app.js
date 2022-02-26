2ba1e71b58c56815e8dbc327d70badd1f7e6e9df
MelihCelik00 <melihsafa.c@gmail.com>
2022-02-26 14:01:57 +0300

    alert pop up and alert dismiss has written, now can add items to the to do list

--- Stuff/TodoList/js/app.js
+++ Stuff/TodoList/js/app.js
@@ -1,14 +1,44 @@
 let inputDOM = document.querySelector("input#task");
 let addButtonDOM = document.querySelector("#liveToastBtn");
+let todoList = document.querySelector("ul#list");
+const alertDOM = document.querySelector("p#alert");
 
-let formDOM = document.querySelector("#todoForm");
-formDOM.addEventListener("click", addItem);
+const toastAlertFunc = () => `
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Hey buddy!</strong> You can't write empty task...
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+`;
+
+const closeAlertInTime = (delayTime = 2000) => {
+    setTimeout(()=>{$(".alert").alert("close")},delayTime);
+};
+    
+
+addButtonDOM.addEventListener("click", addItem);
 
 function addItem(event){
     event.preventDefault();
+    // console.log("clicked!!!");
+    const isEmpty = taskString => !taskString.trim().length;
+    
+    if(isEmpty(inputDOM.value)){
+        // console.log("empty and alert will come!");
+        alertDOM.innerHTML = toastAlertFunc();
+        closeAlertInTime
+    }else{
 
-}
+        // create new li element
+        let liDOM = document.createElement('li');
+        // append task value to li value
+        liDOM.innerHTML = inputDOM.value;
+        // append li to ul dom
+        todoList.appendChild(liDOM);
+        // empty input box
+        inputDOM.innerHTML = "";
+    }
 
-function newElement(){
-    console.log("clicked!!!!");
+    
 }
