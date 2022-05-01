5d256b9ccd656ac8af61c3ee15c41c4ea0afe66f
MelihCelik00 <melihsafa.c@gmail.com>
2022-05-01 14:33:39 +0300

    first assignment done by using process args

--- NodeJS/IntroToNodeJS/assignment1.js
+++ NodeJS/IntroToNodeJS/assignment1.js
@@ -1,2 +1,17 @@
-alert("This is the nodejs branch");
-console.log("now should be merged with a commit message"); 
+/*
+Hepimizin Matematik derslerinden bildiği üzere Dairenin Alanı = π x r2 şeklinde hesaplanır. 
+Node.JS Javascript çalışma ortamında yarıçap değerini konsoldan parametre olarak girerek alanı bulmaya çalışacağız. 
+Konsol çıktısı: Yarıçapı (Yarıçap) olan dairenin alanı: (Alan) şeklinde olmalıdır.
+*/
+
+const arguments = process.argv.slice(2);
+
+function areaOfCircle(radius){
+    let area = Math.PI * ( radius**2 );
+    console.log(area);
+    return area;
+}
+
+areaOfCircle(arguments[0]);
+
+console.log(`Area of circle with the radius ${arguments[0]}: ${areaOfCircle(arguments[0])}`);
