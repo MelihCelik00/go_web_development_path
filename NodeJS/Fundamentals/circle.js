1804e82ec04d3023df79ff53194eaed3a3b6fcf1
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-01 19:41:58 +0300

    learning modules on nodejs

---
+++ NodeJS/Fundamentals/circle.js
@@ -0,0 +1,19 @@
+/*
+Daire Modüler Dosyası
+Daire alan : circleArea ve daire çevre : circleCircumference fonksiyonları içeren ve consola sonuçları yazdıran circle.js dosyası oluşturunuz.
+module.exports yöntemi ile fonksiyonları oluştururken export ediniz.
+require ve object destructing kullanarak index.js dosyasında yarıçap (r) 5 olacak şekilde ekran çıktısını alınız.
+*/
+
+function circleArea(radius){
+    return Math.PI * Math.pow(radius, 2);
+}
+
+function circleCircumference(radius){
+    return 2* Math.PI * radius;
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
