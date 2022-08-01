e9ca711c3ca992e11d49f8a1300a7ee9e90d2cc5
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-01 20:48:29 +0300

    writeFile addition

--- NodeJS/Fundamentals/fsmodule.js
+++ NodeJS/Fundamentals/fsmodule.js
@@ -6,3 +6,8 @@ fs.readFile("./txtfiles/password.txt", 'utf8', (err, res) => {
     console.log(res);
     console.log("data reading is finished!")
 });
+
+fs.writeFile("./txtfiles/example.txt", "New file!", "utf8", (err) => {
+    if (err) console.log(err);
+    console.log("File is created successfully!")
+});
