818b3ce762ccfa96df20ceed45d70b0b35b404cc
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-01 20:57:34 +0300

    some file operations

--- NodeJS/Fundamentals/fsmodule.js
+++ NodeJS/Fundamentals/fsmodule.js
@@ -1,11 +1,15 @@
 // import fs from "fs"; 
 const fs = require("fs");
 
+// Read already created file
+
 fs.readFile("./txtfiles/password.txt", 'utf8', (err, res) => {
     if (err) console.log(err);
     console.log(res);
     console.log("data reading is finished!")
 });
+
+// Create new file
 
 fs.writeFile("./txtfiles/example.txt", "New file!", "utf8", (err) => {
     if (err) console.log(err);
@@ -16,3 +20,17 @@ fs.writeFile("./json/example.json", "{'name': 'Melih', 'age': '21'}", 'utf8', (e
     if (err) console.log(err);
     console.log("Json file is created successfully!")
 });
+
+// Update File
+
+fs.appendFile("./txtfiles/example.txt", "\nSecond row!!!", "utf8", (err) => {
+    if (err) console.log(err);
+    console.log("New data added to the file!!!!")
+});
+
+// Delete file
+
+// fs.unlink("./json/example.json", (err) => {
+//     if (err) console.log(err);
+//     console.log("File is deleted!!!!")
+// });
