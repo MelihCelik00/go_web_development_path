67a1e608928372cc2491415a25e066e53ed71099
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-02 10:56:34 +0300

    some global object practice

---
+++ NodeJS/Fundamentals/index2.js
@@ -0,0 +1,5 @@
+const { circleArea, circleCircumference } = require('./circle');
+
+
+console.log("Area of the circle with the radius (5): ",circleArea(5));
+console.log("Circumference of the circle with the radius (5): ", circleCircumference(5));
