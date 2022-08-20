e386456656bf3a94efc2da1ad376410464f89690
MelihCelik00 <melihsafa.c@gmail.com>
2022-08-20 13:28:55 +0300

    changes are done

--- NodeJS/web_server/own_server_assignment.js
+++ NodeJS/web_server/own_server_assignment.js
@@ -1,8 +1,27 @@
 const http = require('http');
 
-const server = http.createServer(()=>{
+const server = http.createServer((req, res)=>{
     
-    const url = get.url;  // after 'url/'
+    const url = req.url;  // catch after 'url/'
+
+    if(url === '/'){  // index page
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write("<h1>Index Sayfasina Hosgeldiniz</h1>");
+    }
+    else if(url === '/hakkimda'){  // index page
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write("<h2>Hakkimda Sayfasina Hosgeldiniz</h2>");
+    }
+    else if(url === '/iletisim'){  // index page
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write(`<h2>Iletisim Sayfasina Hosgeldiniz</h2>`);
+    }
+    else{
+        res.writeHead(404, {'Content-type':'text/html'});
+        res.write("<h1>404 Not Found.</h1>");
+    }
+
+    res.end();
 })
 
 const port = 5000;
